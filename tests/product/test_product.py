0e92270294a67608c39b744512c3c46069142c4a
Eduardo Muchak <eduardomuchak@gmail.com>
2022-10-29 14:18:45 -0300

    feat: :sparkles: Testar o construtor do objeto e relatorio individual do produto
    Requisitos 1 e 2

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "Café",
+        "Orfeu",
+        "2020-01-01",
+        "2021-01-01",
+        "123456789",
+        "em local seco, fresco e longe de luz",
+    )
+    assert produto.id == 1
+    assert produto.nome_do_produto == "Café"
+    assert produto.nome_da_empresa == "Orfeu"
+    assert produto.data_de_fabricacao == "2020-01-01"
+    assert produto.data_de_validade == "2021-01-01"
+    assert produto.numero_de_serie == "123456789"
+    assert (
+        produto.instrucoes_de_armazenamento
+        == "em local seco, fresco e longe de luz"
+    )
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "Café",
+        "Orfeu",
+        "2020-01-01",
+        "2021-01-01",
+        "123456789",
+        "em local seco, fresco e longe de luz",
+    )
+    assert (
+        produto.__repr__() == "O produto Café"
+        " fabricado em 2020-01-01"
+        " por Orfeu com validade"
+        " até 2021-01-01"
+        " precisa ser armazenado em local seco, fresco e longe de luz."
+    )
